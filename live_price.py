246e0114a3701255da4836c9e87d2bf9bc3f4516
andyno30 <132534797+andyno30@users.noreply.github.com>
2023-09-17 21:58:25 -0700

    Add files via upload

---
+++ live_price.py
@@ -0,0 +1,19 @@
+import yfinance as yf
+
+spx_ticker = "^SPX"
+spy_ticker = "SPY"
+
+# Fetch the live prices
+spx_data = yf.Ticker(spx_ticker).history(period="1min")
+spy_data = yf.Ticker(spy_ticker).history(period="1min")
+
+# Extract the closing prices
+spx_price = spx_data["Close"].iloc[0]
+spy_price = spy_data["Close"].iloc[0]
+
+# Calculate the ratio
+ratio = spx_price / spy_price
+
+# Print the ratio (you can also store it in a variable or file)
+print(f"SPX/SPY Ratio: {ratio:.7f}")
+
