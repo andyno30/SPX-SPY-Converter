5d27a2ed20434fd412134a42cc288d8ba9dfb631
andyno30 <132534797+andyno30@users.noreply.github.com>
2023-09-17 22:06:41 -0700

    Update live_price.py

--- live_price.py
+++ live_price.py
@@ -1,18 +1,26 @@
 import yfinance as yf
+from flask import Flask, jsonify
 
-spx_ticker = "^SPX"
-spy_ticker = "SPY"
+app = Flask(__name__)
 
-# Fetch the live prices
-spx_data = yf.Ticker(spx_ticker).history(period="1min")
-spy_data = yf.Ticker(spy_ticker).history(period="1min")
+@app.route("/get_live_ratio", methods=["GET"])
+def get_live_ratio():
+    spx_ticker = "^SPX"
+    spy_ticker = "SPY"
 
-# Extract the closing prices
-spx_price = spx_data["Close"].iloc[0]
-spy_price = spy_data["Close"].iloc[0]
+    # Fetch the live prices
+    spx_data = yf.Ticker(spx_ticker).history(period="1min")
+    spy_data = yf.Ticker(spy_ticker).history(period="1min")
 
-# Calculate the ratio
-ratio = spx_price / spy_price
+    # Extract the closing prices
+    spx_price = spx_data["Close"].iloc[0]
+    spy_price = spy_data["Close"].iloc[0]
 
-print(f"SPX/SPY Ratio: {ratio:.7f}")
+    # Calculate the ratio
+    ratio = spx_price / spy_price
 
+    # Return the ratio as JSON
+    return jsonify({"liveRatio": ratio})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
