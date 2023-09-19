461d394253c1a73d428d1bd1c2f0287373cc8183
andyno30 <132534797+andyno30@users.noreply.github.com>
2023-09-19 09:35:16 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
-const conversionRatio = 4124.08 / 411.59;
-
-function convertSpxToSpy() {
-    const spxValue = parseFloat(document.getElementById("spx-input").value);
-    const spyValue = spxValue / conversionRatio;
-    document.getElementById("spy-result").innerText = `SPY: ${spyValue.toFixed(2)}`;
+// Function to fetch the ratio from the server
+function fetchRatio() {
+    fetch('/get_live_price')
+        .then(response => response.json())
+        .then(data => {
+            const ratio = data.ratio;
+            document.getElementById("ratio").textContent = `Conversion ratio last updated: ${new Date().toLocaleDateString()} - SPX/SPY Ratio: ${ratio.toFixed(7)}`;
+        })
+        .catch(error => {
+            console.error('Error fetching ratio:', error);
+        });
 }
 
+// Initial fetch of the ratio when the page loads
+fetchRatio();
+
+// Function to convert SPX to SPY
+function convertSpxToSpy() {
+    const spxValue = parseFloat(document.getElementById("spxInput").value);
+    const ratio = parseFloat(document.getElementById("ratio").textContent.split(":")[1].trim());
+    const spyValue = (spxValue / ratio).toFixed(2);
+    document.getElementById("spxToSpyOutput").innerText = `SPY Value: ${spyValue}`;
+}
+
+// Function to convert SPY to SPX
 function convertSpyToSpx() {
-    const spyValue = parseFloat(document.getElementById("spy-input").value);
-    const spxValue = spyValue * conversionRatio;
-    document.getElementById("spx-result").innerText = `SPX: ${spxValue.toFixed(2)}`;
+    const spyValue = parseFloat(document.getElementById("spyInput").value);
+    const ratio = parseFloat(document.getElementById("ratio").textContent.split(":")[1].trim());
+    const spxValue = (spyValue * ratio).toFixed(2);
+    document.getElementById("spyToSpxOutput").innerText = `SPX Value: ${spxValue}`;
 }
