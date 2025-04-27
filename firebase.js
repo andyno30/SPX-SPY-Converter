3241128467406827be0046dbddba1646d1ade690
andyno30 <132534797+andyno30@users.noreply.github.com>
2025-04-27 22:18:22 +0900

    Delete firebase.js

--- firebase.js
+++
@@ -1,5 +0,0 @@
-const functions = require("firebase-functions");
-const loginBackend = require("./loginBackend");
-
-exports.createStripeCheckout = functions.https.onRequest(loginBackend.createStripeCheckout);
-exports.handleStripeWebhook = functions.https.onRequest(loginBackend.handleStripeWebhook);
