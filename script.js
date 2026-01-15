828d8f72d97ab2364a802baefb91a47d13c29561
andyno30 <132534797+andyno30@users.noreply.github.com>
2026-01-15 07:40:50 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Global variable to hold the SPX/SPY Ratio
-let ratio = 10.035166240409207; // Initial value, will be updated
+let ratio = 10.034746811756916; // Initial value, will be updated
 
 // Function to fetch and update the SPX/SPY Ratio
 function updateRatio() {
