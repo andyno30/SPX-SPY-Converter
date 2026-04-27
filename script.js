4f0917b93d00a7ea595bd692514ef6543c3d109c
andyno30 <132534797+andyno30@users.noreply.github.com>
2026-04-27 06:36:53 -0700

    AutoSync2: update /script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Global variable to hold the SPX/SPY Ratio
-let ratio = /* RATIO */ 10.035057840247433;
+let ratio = /* RATIO */ 10.0325097347116;
 
 // Function to fetch and update the SPX/SPY Ratio
 function updateRatio() {
